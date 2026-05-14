6babd8e63eb1321226440179afdceb0dbe6391ca
Oleh Kit <oleh.kit@student.tuke.sk>
2026-05-14 10:32:02 +0200

    refactor

--- Insta_Parser/main_parse.py
+++ Insta_Parser/main_parse.py
@@ -6,16 +6,21 @@ from Insta_Parser.scanner import process_scanner
 from Insta_Parser.worker import process_worker
 
 async def main_process(app):
-    all_accounts = await db.get_accounts()
-    
-    for i, account in enumerate(all_accounts):
-        shortcodes = await db.get_shortcodes(account_id=account['id'])
-        all_accounts[i]['shortcodes'] = shortcodes
+    pending_count = await db.get_pending_posts()
+    if pending_count > 0:
+        print(f"[SKIP] В очереди еще {pending_count} постов. Пропускаем сканирование, чтобы не спамить.")
+    else:
 
-    await process_scanner(all_accounts=all_accounts, max_taken=MAX_TAKEN)
+        all_accounts = await db.get_accounts()
+    
+        for i, account in enumerate(all_accounts):
+            shortcodes = await db.get_shortcodes(account_id=account['id'])
+            all_accounts[i]['shortcodes'] = shortcodes
+
+        await process_scanner(all_accounts=all_accounts, max_taken=MAX_TAKEN)
 
     wait_time = random.uniform(100, 200)
-    await asyncio.sleep(wait_time)
     print(f"[SCAN] Пауза {wait_time:.1f} сек.")
+    await asyncio.sleep(wait_time)
 
     await process_worker(app, LIMIT_POSTS)
